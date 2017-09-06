7cdd5aae59ca3ff5bfe26ce8dd11fcf67150e186
HJ Park <hj.park@ims-elec.com>
2017-09-06 10:08:59 +0900

    Create install_python_opencv.sh

---
+++ install_python_opencv.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+sudo apt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install libopencv-dev
+sudo apt-get install python3-numpy
+sudo apt-get install python3-matplotlib.
+sudo apt-get install python-OpenCV
+sudo apt install python3-pip
+
+git clone https://github.com/Itseez/opencv.git
+pip3 install --upgrade pip
+pip3 install numpy
+sudo pip3 install opencv-python
+pip3 install numpy
+sudo pip3 install matplotlib 
