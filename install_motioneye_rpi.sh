2de4068973da0bbcd243557c6920c486d9daa03d
HJ Park <hj.park@ims-elec.com>
2018-04-02 14:58:13 +0900

    add -y

--- install_motioneye_rpi.sh
+++ install_motioneye_rpi.sh
@@ -1,11 +1,11 @@
-sudo apt-get update
-sudo apt-get install ffmpeg v4l-utils
-sudo apt-get install libmariadbclient18 libpq5
+sudo apt-get -y update
+sudo apt-get -y install ffmpeg v4l-utils
+sudo apt-get -y install libmariadbclient18 libpq5
 wget https://github.com/Motion-Project/motion/releases/download/release-4.1.1/pi_stretch_motion_4.1.1-1_armhf.deb
 
 
 sudo dpkg -i pi_stretch_motion_4.1.1-1_armhf.deb
-sudo apt-get install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev
+sudo apt-get -y install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev
 sudo pip install motioneye
 sudo mkdir -p /etc/motioneye
 sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
